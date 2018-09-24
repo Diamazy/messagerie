bd556180f82c9fd3081c600c575253940d5b86e0
diamazy <diamazy@gmail.com>
2018-09-24 12:36:13 +0200

    msg

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    die("ok");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Messagerie</title>
+</head>
+<body>
+    <h1>page index</h1>
+</body>
+</html>
+
