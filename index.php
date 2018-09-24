472814fd3ed9fa83907923be06e7d87fd183dab2
diamazy <diamazy@gmail.com>
2018-09-24 14:08:28 +0200

    go

--- index.php
+++ index.php
@@ -13,7 +13,19 @@ if(isset($_GET['page']) && !empty($_GET['page'])){
     <title>Messagerie</title>
 </head>
 <body>
-    <h1>page index</h1>
+ <div id="main">
+     <div id="info">
+         <h2>Login here</h2>
+         <form action="login.php" method="post">
+             <label> <b>Username</b></label>
+             <input type="text" name="uname" placeholder="User name"><br><br>
+             <label> <b>Password</b></label>
+             <input type="text" nama="pass" placeholder="Password"> <br><br>
+             <button style="background-color: #6495ed; color: white;" type="submit"><b>Login</b>
+            </button>
+         </form>
+     </div>
+ </div>
 </body>
 </html>
 
